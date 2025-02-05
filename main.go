ff7d8707c569391bc6ee55a6ee0007b38231d517
rustam <rustamaxm@gmail.com>
2025-02-05 10:46:12 +0300

    gorutine try

--- main.go
+++ main.go
@@ -1,11 +1,26 @@
 package main
 
-
-import "fmt"
-import "go-education/my_module"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
 func main() {
-    fmt.Println("Hello, Go!")
-    m := my_module.NewModule()
-    m.Module_func()
+	wg := new(sync.WaitGroup)
+
+	for i := 0; i < 5; i++ {
+		wg.Add(1) // Увеличиваем счетчик горутин в группе
+		go work(i, wg) // Вызываем функцию work в отдельной горутине
+	}
+
+	wg.Wait() // ожидаем завершения всех горутин в группе
+	fmt.Println("all gorutine done")
+}
+
+func work(id int, wg *sync.WaitGroup)  {
+	defer wg.Done()
+	fmt.Printf("gorutine %d start \n", id)
+	time.Sleep(2 * time.Second)
+	fmt.Printf("gorutine %d stoped \n", id)
 }
