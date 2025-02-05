b3a35d770902ecf666a7adb53b2e8b038e8d8d95
rustam <rustamaxm@gmail.com>
2025-02-05 11:03:07 +0300

    ticker test

--- main.go
+++ main.go
@@ -7,20 +7,23 @@ import (
 )
 
 func main() {
+	tick := time.NewTicker(time.Second)
+	defer tick.Stop()
+
 	wg := new(sync.WaitGroup)
 
 	for i := 0; i < 5; i++ {
 		wg.Add(1) // Увеличиваем счетчик горутин в группе
-		go work(i, wg) // Вызываем функцию work в отдельной горутине
+		go work(i, tick.C, wg) // Вызываем функцию work в отдельной горутине
 	}
 
 	wg.Wait() // ожидаем завершения всех горутин в группе
 	fmt.Println("all gorutine done")
 }
 
-func work(id int, wg *sync.WaitGroup)  {
+func work(id int, limit <- chan time.Time, wg *sync.WaitGroup)  {
 	defer wg.Done()
 	fmt.Printf("gorutine %d start \n", id)
-	time.Sleep(2 * time.Second)
+	<- limit 
 	fmt.Printf("gorutine %d stoped \n", id)
 }
